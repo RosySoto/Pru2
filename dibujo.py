f4d1ce0c62633f706b6a778b364192f4ac5fcd62
Ross <ros.soto99@gmail.com>
2019-10-16 11:06:00 -0500

    Primera version

---
+++ dibujo.py
@@ -0,0 +1,16 @@
+__author__ = 'kaue'
+import turtle,time
+colors = ['red', 'yellow', 'blue', 'green', 'yellow', 'orange', 'pink']
+t1=turtle.Pen()
+turtle.bgcolor('black')
+t1.speed(0)
+for x in range(1000):
+    t1.pencolor(colors[x%7])
+    if x%2==0:
+        t1.forward(x)
+    else:
+        t1.backward((x-1))
+    t1.left(52)
+
+
+time.sleep(30)
